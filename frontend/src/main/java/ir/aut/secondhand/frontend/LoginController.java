b9f404d6d7ea84cc4413138a7f53a8915efa2821
Amirreza Ghasemi <amirrezaghasemi4911@gmail.com>
2026-07-01 23:07:17 +0330

    fix:Maven and JavaFX configuration feat:Create login page UI feat(frontend):implement initial login UI

--- frontend/src/main/java/ir/aut/secondhand/frontend/LoginController.java
+++ frontend/src/main/java/ir/aut/secondhand/frontend/LoginController.java
@@ -1,4 +1,22 @@
 package ir.aut.secondhand.frontend;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 public class LoginController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void login(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
 }
