0ac9d70a9365d35a2cc916c52d64b46b1781a7ef
Amirreza Ghasemi <amirrezaghasemi4911@gmail.com>
2026-07-03 20:00:07 +0330

    style:separate auth and app page backgrounds feat:improve add advertisement navigation and styling

--- frontend/src/main/java/ir/aut/secondhand/frontend/AddAdvertisementController.java
+++ frontend/src/main/java/ir/aut/secondhand/frontend/AddAdvertisementController.java
@@ -1,10 +1,16 @@
 package ir.aut.secondhand.frontend;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddAdvertisementController {
 
@@ -25,6 +31,9 @@ public class AddAdvertisementController {
 
     @FXML
     private ImageView imagePreview;
+
+    @FXML
+    private Button backButton;
 
     @FXML
     public void initialize() {
@@ -51,7 +60,20 @@ public class AddAdvertisementController {
     }
 
     @FXML
-    private void goBack() {
-        System.out.println("Back clicked");
+    private void goBack() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ir/aut/secondhand/frontend/fxml/home-view.fxml"));
+
+        Stage stage = (Stage) backButton.getScene().getWindow();
+
+        boolean maximized = stage.isMaximized();
+        double width = stage.getWidth();
+        double height = stage.getHeight();
+
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+
+        scene.getStylesheets().add(getClass().getResource("/ir/aut/secondhand/frontend/css/style.css").toExternalForm());
+
+        stage.setScene(scene);
+        stage.setMaximized(maximized);
     }
 }
