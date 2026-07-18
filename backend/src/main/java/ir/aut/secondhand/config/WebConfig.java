dd81be75ee29f944ab7ee4ffa05ee1264ddda672
Ehsan Ahmadi <ehahmedia@gmail.com>
2026-07-18 22:56:26 +0330

    chore: update dependencies and config image upload

---
+++ backend/src/main/java/ir/aut/secondhand/config/WebConfig.java
@@ -0,0 +1,23 @@
+package ir.aut.secondhand.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.upload.advertisements-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String absolutePath = Paths.get(uploadDir).toAbsolutePath().toUri().toString();
+        
+        registry.addResourceHandler("/uploads/advertisements/**")
+                .addResourceLocations(absolutePath);
+    }
+}
